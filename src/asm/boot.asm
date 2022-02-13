42a0b3e9a1014e6ce1133cd7e7c8e8ef56c2abea
MelvinSam2000 <melvinsamxd@gmail.com>
2022-02-13 13:40:08 -0500

    Boot with opensbi

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -2,9 +2,6 @@
 .global _start
 
 _start:
-    // run only hart 0
-    csrr    t0, mhartid
-    bnez    t0, park
 
     // set gp pointer
     la      gp, _global_pointer
@@ -15,29 +12,13 @@ _start:
     // clear bss section
     call    clear_bss
     
-    // set cpu to machine mode
-    li		t0, (0b11 << 11) | (1 << 7) | (1 << 3)
-    csrw	mstatus, t0
-
-    // set machine expection to kmain
-    la		t1, kmain
-    csrw	mepc, t1
-    
     // set trap vector csr
     la		t2, trap_vector
-    csrw	mtvec, t2
-    
-    // enable interrupts
-    li		t3, (1 << 3) | (1 << 7) | (1 << 11)
-    csrw	mie, t3
+    csrw	stvec, t2
 
-    // go to Rust (kmain) 
+    // go to Rust (kmain)
     la		ra, park
-    mret
-
-park:
-    wfi
-    j       park
+    call    kmain
 
 clear_bss:
     la      t0, _bss_start
